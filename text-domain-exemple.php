66f0c5ca2461200b130aa1c6fd728964e32873d7
Samuel da Cruz <51212692+samucadacruz@users.noreply.github.com>
2021-07-20 13:52:48 -0300

    Create text-domain-exemple.php

---
+++ text-domain-exemple.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Plugin Name: API Requets
+* Plugin URI: https://firehospedagem.com
+* Description: Requisição de API Rest
+* Version: 1.0.0
+* Author: Your Name
+* Author URI: https://firehospedagem.com
+* License: GPL2
+*/
+
+add_action( 'o-titulo', 'text-domain-exemple' );
+
+function text-domain-exemple() {
+  _e( 'Helo World' );
+
+}
+
+?>
